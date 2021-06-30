d227ee9d4a48b671dc5af5f31ba495ca34d085a7
jjimini98 <jjimini98@naver.com>
2021-06-30 11:34:03 +0900

    Java TPC 3강 완료

---
+++ src/kr/bit/Book.java
@@ -0,0 +1,14 @@
+package kr.bit; // 패키지 선언문
+
+// 책(객체) -> 책 제목, 가격, 출판사, 페이지 수, 지은이... (책의 상태 정보들)
+// 행위정보 -> 메소드(동작), 여기선 변수가 5개 필요
+public class Book { // 새로운 자료형
+
+    public String title;
+    public int price;
+    public String company;
+    public int page;
+    public String author ;
+
+
+}
