591d30f19bfa6324b962aec4d5b04cb1cefc8e58
jjimini98 <jjimini98@naver.com>
2021-06-30 10:10:19 +0900

    Java TPC 1강 완료

---
+++ src/TPC01.java
@@ -0,0 +1,9 @@
+public class TPC01 {
+    //메인 메소드 단축키 psvm + enter
+    public static void main(String[] args) {
+        //System.out.println() 단축키 : sout + tab
+        System.out.println();
+    }
+
+
+}
