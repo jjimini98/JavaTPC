759834507a5afda8a294864ca291d38f8c1c5754
jjimini98 <jjimini98@naver.com>
2021-06-30 10:59:06 +0900

    Java TPC 2강 완료

---
+++ src/TPC02.java
@@ -0,0 +1,26 @@
+public class TPC02 {
+    public static void main(String[] args) {
+        //프로그래밍의 3대 요소 : 변수, 자료형(Data Type) , 할당 ( = )
+
+        int a,b,c;
+        a = 1;
+        b = 1;
+        c = a+b;
+        System.out.println(c);
+
+
+        float f ; //실수형 데이터는 float와 double이 있는데 자바는 기본적으로 double로 생각. float로 하려면 뒤에 표시
+        f = 34.5f;
+        System.out.println(f);
+
+        char e ;
+        e = 'E';
+        System.out.println(e);
+
+        boolean g;
+        g = true; //true 는 참 false는 거짓
+        System.out.println(g);
+
+    }
+
+}
